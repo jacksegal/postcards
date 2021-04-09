98a7e4daa82d77e2bbcfe954303dbd801c383c76
Jack Segal <jackbsegal@gmail.com>
2021-04-09 11:46:25 +0100

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,6 +57,7 @@
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
                                 </div>
+                                <hr>
                             </div>
                         </div>
 
