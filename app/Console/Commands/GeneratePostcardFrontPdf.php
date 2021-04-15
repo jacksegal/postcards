f35510b68864b6c20f343dbbb39e3a872fabdd4f
Christoph Rumpel <c.rumpel@kabsi.at>
2021-04-15 10:50:53 +0200

    Refactor generate pdf command to use pdf helper class

--- app/Console/Commands/GeneratePostcardFrontPdf.php
+++ app/Console/Commands/GeneratePostcardFrontPdf.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Postcards\PdfHelper;
 use Dompdf\Dompdf;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
@@ -18,19 +19,14 @@ class GeneratePostcardFrontPdf extends Command
         $imagePath = public_path('images/' . $this->argument('imageName') . '.png');
         $imageUrl = asset('images/' . $this->argument('imageName') . '.png');
 
-        throw_if(!File::exists($imagePath),\ErrorException::class, 'Image does not exist here: ' .$imagePath);
+        throw_if(!File::exists($imagePath), \ErrorException::class, 'Image does not exist here: ' . $imagePath);
 
         $html = view('pdf.template', ['image' => $imageUrl])->render();
 
-        $dompdf = new Dompdf();
-        $options = $dompdf->getOptions();
-        $options->setIsRemoteEnabled(true);
-        $dompdf->setOptions($options);
-        $dompdf->loadHtml($html);
-        $dompdf->setPaper('A5', 'landscape');
-        $dompdf->render();
-        $output = $dompdf->output();
+        $pdfHelper = new PdfHelper();
+        $pdfHelper->useHtml($html)
+            ->path(public_path('pdfs/' . $this->argument('imageName') . '.pdf'))
+            ->create();
 
-        File::put(public_path('pdfs/' . $this->argument('imageName') . '.pdf'), $output);
     }
 }
---
+++ app/Postcards/PdfHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Postcards;
+
+
+use Dompdf\Dompdf;
+use Illuminate\Support\Facades\File;
+
+class PdfHelper
+{
+    private string $html;
+
+    private string $path;
+
+    public function useHtml(string $html): self
+    {
+        $this->html = $html;
+
+        return $this;
+    }
+
+    public function path(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function create(): void
+    {
+        $dompdf = app(Dompdf::class);
+        $options = $dompdf->getOptions();
+        $options->setIsRemoteEnabled(true);
+        $dompdf->setOptions($options);
+        $dompdf->loadHtml($this->html);
+        $dompdf->setPaper('A5', 'landscape');
+        $dompdf->render();
+        $output = $dompdf->output();
+
+        File::put($this->path, $output);
+    }
+}
