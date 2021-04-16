b4901b49ffe67fbb1249f79555bd7cb1e814a494
Christoph Rumpel <c.rumpel@kabsi.at>
2021-04-16 14:16:47 +0200

    Add test image and clean up code

--- tests/Feature/GeneratePostcardFrontPdfTest.php
+++ tests/Feature/GeneratePostcardFrontPdfTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use ErrorException;
+use Illuminate\Support\Facades\File;
 use Tests\TestCase;
 
 class GeneratePostcardFrontPdfTest extends TestCase
@@ -10,16 +12,19 @@ class GeneratePostcardFrontPdfTest extends TestCase
     public function it_creates_pdf_from_running_artisan_command(): void
     {
         // Act
-    	$this->artisan('postcards:generate-front-pdf postcard-front-bycatch');
+    	$this->artisan('postcards:generate-front-pdf postcard-test-front-image');
 
     	// Assert
-        $this->assertFileExists(public_path('pdfs/postcard-front-bycatch.pdf'));
+        $this->assertFileExists(public_path('pdfs/postcard-test-front-image.pdf'));
+
+        // Clean up
+        File::delete(public_path('pdfs/postcard-test-front-image.pdf'));
     }
 
     /** @test **/
     public function it_throws_error_if_image_not_given(): void
     {
-        $this->expectException(\ErrorException::class);
+        $this->expectException(ErrorException::class);
 
         // Act
         $this->artisan('postcards:generate-front-pdf image-not-given');
